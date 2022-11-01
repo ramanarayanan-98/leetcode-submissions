3a3f4f44cd4e6a40193dd2a9ec91947fa2ff5936
Ramanarayanan Sankaranarayanan <113214177+ramanarayanan-98@users.noreply.github.com>
2022-11-01 14:32:20 -0400

    Time: 1350 ms (57.55%), Space: 14 MB (72.64%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -1,13 +1,14 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
         n = len(arr)
-        cache = [0]*(n+1)
+        cache = [0]*k
         
         for i in range(1,n+1):
-            curmax = 0
+            curmax,best = 0,0
             for j in range(1,min(k,i)+1):
                 curmax = max(curmax,arr[i-j])
-                cache[i] = max(cache[i],cache[i-j] + (curmax*j))
+                best = max(best,cache[(i-j)%k] + (curmax*j))
+            cache[i%k] = best
         
-        return cache[n]
+        return cache[n%k]
             
