0970687fb602a5432e23911a4fbb7054f0c1dd2c
Ramanarayanan Sankaranarayanan <113214177+ramanarayanan-98@users.noreply.github.com>
2022-11-07 01:10:49 -0500

    Time: 2863 ms (5.03%), Space: 16.2 MB (7.16%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -0,0 +1,33 @@
+from sortedcontainers import SortedDict
+class Solution:
+    def decrementCount(self,itemcounts,key):
+        if key not in itemcounts:
+            return
+        itemcounts[key] -= 1
+        
+        if not itemcounts[key]:
+            del itemcounts[key]
+            
+            
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand)%groupSize:
+            return False
+        itemcounts = SortedDict()
+        for num in hand:
+            itemcounts[num] = itemcounts.get(num,0) + 1
+        
+        while len(itemcounts):
+            curkey = list(itemcounts.keys())[0]
+            self.decrementCount(itemcounts,curkey)
+            count = 1
+            while count < groupSize and curkey+1 in itemcounts:
+                count += 1
+                self.decrementCount(itemcounts,curkey+1)
+                curkey += 1
+            if count != groupSize:
+                return False
+        
+        return True
+                
+            
+            
