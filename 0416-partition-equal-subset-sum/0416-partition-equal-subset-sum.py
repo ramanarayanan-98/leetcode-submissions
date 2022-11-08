984385dcb01a244e86e7dbc8f1bce8f136c92290
Ramanarayanan Sankaranarayanan <113214177+ramanarayanan-98@users.noreply.github.com>
2022-11-08 05:41:02 -0500

    Time: 5884 ms (18.09%), Space: 13.9 MB (97.29%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -5,15 +5,12 @@ class Solution:
         if totalsum%2:
             return False
         totalsum //= 2
-        cache = [[False for _ in range(totalsum+1)] for _ in range(n+1)]
-        for i in range(n+1):
-            cache[i][0] = True
+        cache = [False for _ in range(totalsum+1)]
+        cache[0] = True
         
         for i in range(1,n+1):
-            for j in range(1,totalsum+1):
-                if j < nums[i-1]:
-                    cache[i][j] = cache[i-1][j]
-                else:
-                    cache[i][j] = cache[i-1][j] | cache[i-1][j-nums[i-1]]
-        return cache[-1][-1]
+            for j in range(totalsum,-1,-1):
+                if j >= nums[i-1]:
+                    cache[j] |= cache[j-nums[i-1]]
+        return cache[-1]
         
