56eea5fdf4ec62f2799409248720ec8b11eb53a2
Ramanarayanan Sankaranarayanan <113214177+ramanarayanan-98@users.noreply.github.com>
2022-11-08 21:14:11 -0500

    Time: 190 ms (27.74%), Space: 14 MB (93.31%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,14 +5,12 @@ class Solution:
         start,end = 0,0
         
         for end in range(0,len(s)):
-            if s[end] not in curset:
-                curset.add(s[end])
-                maxLen = max(maxLen,len(curset))
-                continue
-            while start < end and s[end] in curset:
-                curset.remove(s[start])
-                start += 1
+            if s[end] in curset:
+                while start < end and s[end] in curset:
+                    curset.remove(s[start])
+                    start += 1
             curset.add(s[end])
+            maxLen = max(maxLen,len(curset))
         
         return maxLen
             
