cb4ff69ff015ae5002d97ab1ac6d0ab59f28b2d6
Ramanarayanan Sankaranarayanan <113214177+ramanarayanan-98@users.noreply.github.com>
2022-11-20 04:51:03 -0500

    Time: 131 ms (30.56%), Space: 14.5 MB (28.10%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,17 +1,37 @@
-class Solution:
-    def dfs(self,source,target,seen,graph)->bool:
-        if source in seen:
+class DSU:
+    def __init__(self):
+        self.parent = list(range(1001))
+        self.rank = [0 for _ in range(1001)]
+    
+    def find(self,x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self,a,b) -> bool:
+        a_lead,b_lead = self.find(a),self.find(b)
+        
+        if a_lead == b_lead:
             return False
-        if source == target:
-            return True
-        seen.add(source)
-        return any(self.dfs(nei,target,seen,graph) for nei in graph[source])
+        
+        if self.rank[a_lead] > self.rank[b_lead]:
+            self.parent[b_lead] = a_lead
+        elif self.rank[b_lead] > self.rank[a_lead]:
+            self.parent[a_lead] = b_lead
+        else:
+            self.rank[a_lead] += 1
+            self.parent[b_lead] = a_lead
+        return True
+        
+class Solution:
+    
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        graph = collections.defaultdict(list)
-        for u,v in edges:
-            seen = set()
-            if u in graph and v in graph and self.dfs(u,v,seen,graph):
-                return [u,v]
-            graph[u].append(v)
-            graph[v].append(u)
+        dsu = DSU()
+        for edge in edges:
+            if not dsu.union(*edge):
+                return edge
+        
+        return [-1,-1]
+            
+        
         
